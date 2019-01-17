b3d79231522f6449f0725a6911bb1f0449823225
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-01-17 08:04:48 +0000

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -18,7 +18,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    const todo = new Todo({text: req.body.text});
+    const todo = new Todo({
+        text: req.body.text
+    });
     todo.save().then(doc => {
         res.send(doc);
     }, (error) => {
@@ -29,3 +31,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, (req, res) => {
     console.log('Started on port 3000');
 });
+
+module.exports = {
+    app
+};
