270aa0ecd73438679e2b5bc89411d7a508ad587e
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-02-19 15:52:20 +0000

    Update mongoose connect options

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    user: 'admin',
+    pass: 'admin'
 });
 
 module.exports = {
